c3209d34235b52f4869adf640c474232f10ca9ef
Shan9413 <159189394+Shan9413@users.noreply.github.com>
2024-02-07 03:40:22 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+
+API_ID = API_ID = 20088962
+
+API_HASH = os.environ.get("API_HASH", "257f47d347157555890a64b12bc0134f")
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6835390216:AAHPv6Zr0PX4q9XxuPGe8kYNGYGk-oVeAXE")
+
+PASS_DB = int(os.environ.get("PASS_DB", "721"))
+
+OWNER = int(os.environ.get("OWNER", 6169016546))
+
+LOG = -1001784009370
+
